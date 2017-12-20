0f17366697ac6bb862b8cb7254dc5c2b95700185
Easwari Ramani <eshuprad@gmail.com>
2017-12-20 11:00:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+    const lowCaseName=drivers.map(function(d){
+  return d.toLowerCase();
+});
+  return lowCaseName;
+}
+function nameToAttributes(drivers)
+{
+    const nameAttrib=drivers.map(function(nm)
+    {
+      const arrNam=nm.split(" ");
+      return {'firstName':arrNam[0],'lastName':arrNam[1]};
+    });
+    return nameAttrib;
+}
+function attributesToPhrase(drivers){
+
+}
